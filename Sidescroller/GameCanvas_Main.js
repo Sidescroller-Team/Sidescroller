80b63797bd1160b636805de10c3ec68d36d76f31
Nicky G <niklas.gieretz@edu.fhdw.de>
2019-10-29 16:04:06 +0100

    NG: Erstellung Grundlegende Struktur

---
+++ Sidescroller/GameCanvas_Main.js
@@ -0,0 +1,62 @@
+﻿var canvas;
+var ctx;
+
+var countBlocksX; 
+var countBlocksY;
+
+var blockSizeX;
+var blockSizeY;
+
+function init() {
+   
+    setVariables();
+    addListener();
+
+    draw();
+    console.log("initfinished");
+
+}
+
+function addListener() {
+    window.addEventListener('resize', function () {
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
+    })
+}
+
+function setVariables() {
+    canvas = document.getElementById("mainCan");
+    ctx = canvas.getContext('2d');
+    countBlocksX = 32;
+    countBlocksY = 24;
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    blockSizeX = ctx.innerWidth / countBlocksX;
+    blockSizeY = ctx.innerHeight / countBlocksY;
+
+}
+
+
+
+function update() {
+
+}
+
+function draw() {
+
+    for (var i = 0; i < countBlocksY; i++) {
+        ctx.fillStyle = "#000000";
+        ctx.fillRect(blockSizeX * i, 200, 25, 25);
+        console.log(i);
+    }
+
+  
+}
+
+function gameLoop() {
+
+//    update();
+    draw();
+
+    setTimeout(gameLoop, 20);
+}

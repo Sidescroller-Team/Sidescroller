432a357dce18786ec72511f0bfe8be25c589e62c
Timo Schallenberg <Timo.Schallenberg@edu.fhdw.de>
2019-11-07 16:04:46 +0100

    Anfang

---
+++ Sidescroller/javascript/PlayerCharacter.js
@@ -0,0 +1,31 @@
+﻿
+main_character = {
+    height: 2 * blockSizeY,
+    width: 1 * blockSizeX,
+    y_position: 1 * blockSizeY,
+    x_position: 10 * blockSizeX,
+    x_pace: 0,
+    y_pace: 0,
+
+}
+
+var shift = 0;
+
+document.addEventListener('keydown', function (evt) {
+    console.log(evt.keyCode);
+    switch (evt.keyCode) {
+
+        case 37:
+            console.log("Pfeiltaste nach links");
+            shift += speed;
+            backgroundPosition -= backgroundSpeed;
+            return false;
+            break;
+        case 39:
+            console.log("Pfeiltaste nach rechts");
+            shift -= speed;
+            backgroundPosition += backgroundSpeed;
+            return false;
+            break;
+    }
+})

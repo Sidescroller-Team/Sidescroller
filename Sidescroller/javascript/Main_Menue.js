a396fd9886a4697768a8c5f3ff5193102efbcf22
Niklas Klaes <Niklas.Klaes@edu.fhdw.de>
2019-11-11 10:24:54 +0100

    NK_Einbau Hauptmenü

---
+++ Sidescroller/javascript/Main_Menue.js
@@ -0,0 +1,25 @@
+﻿
+function init() {
+    setConfigs();
+    fillImages();
+    console.log("images loaded");
+    loadMainMenue();
+}
+
+function loadMainMenue() {
+            console.log("load");
+             loop();
+            
+}
+
+function draw() {
+    ctx.clearRect(0, 0, 1000, 200);
+    console.log("draw");
+    ctx.drawImage(mainMenue, 0, 0);
+   
+}
+
+function loop() {
+    draw();
+   setTimeout(loop, 100);
+}

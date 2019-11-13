3a407fa3a811079ef4449d00175e76926a0cef8d
Niklas Klaes <Niklas.Klaes@edu.fhdw.de>
2019-11-13 10:35:37 +0100

    NK: Anpassung einer Variablen

--- Sidescroller/javascript/MoveObjects.js
+++ Sidescroller/javascript/MoveObjects.js
@@ -1,12 +1,14 @@
-﻿// Damit dies wirklich funnktioniert, muss StartX beim Aufruf durch eine Variable erfolgen. 
+﻿var canvasWidth;
+
+// Damit dies wirklich funnktioniert, muss StartX beim Aufruf durch eine Variable erfolgen.
 // Dieser Variablen muss auch jedes mal das Ergebnis dieser Funktion zugewiesen werden. Siehe Bsp. unten.
 //  startXCloudTwo = paintMovingObject(startXCloudTwo, startYCloudTwo, wolkenformatZwei, startXCloudTwoCopy, shiftCloudTwo);
-// Falls die Breite des Canvas noch geändert wird, muss dies hier angepasst werden (ctx.width funktioniert nicht !!!)
 function paintMovingObject(startX, startY, objectImage, startXCopy, speed) {
+    canvasWidth = countBlocksX * blockSizeX;
     ctx.drawImage(objectImage, startX, startY);
 
-    if ((startX + objectImage.width) >= 1600) {
-        startXCopy = startX - 1600;
+    if ((startX + objectImage.width) >= canvasWidth) {
+        startXCopy = startX - canvasWidth;
         ctx.drawImage(objectImage, startXCopy, startY);
         if (startX >= 1600) {
             startX = startXCopy;

3eea07b4bc14985c80d6b014d7be59fbcea0ed1c
Nicky G <niklas.gieretz@edu.fhdw.de>
2019-11-19 16:54:50 +0100

    Position bleibt beim "Gegenlaufen" gleich, Springen an Wände, kleinere Veränderungen, neue Bilder

--- Sidescroller/javascript/PlayerCharacter.js
+++ Sidescroller/javascript/PlayerCharacter.js
@@ -1,4 +1,5 @@
 ﻿
+
 main_character = {
     height: 0,
     width: 0,
@@ -8,14 +9,20 @@ main_character = {
     jumpingpower: 0,
     jumping: false,
     add_jumpingpower: 0,
-    alive: true
+	alive: true,
+	fallingLeft: false,
+	fallingRight: false,
+	normalGravity: 3,
+	gravity: 3,
+
 }
 
 controller = {
     left: false,
     right: false,
     up: false,
-    blockPermanentJump: false
+	blockPermanentJump: false,
+	forward: true
 }
 
 var frame_running = 0;
@@ -31,16 +38,18 @@ function setPlayerData() {
 
 //wenn eine Taste gedrückt wird, wird geprüft, welche
 document.addEventListener('keydown', function (evt) {
-   // console.log(evt.keyCode);
+
     switch (evt.keyCode) {
-        case 37:
-            controller.left = true;
-            break;
-        case 38:
-            controller.up = true;
-            break;
+		case 37:
+			controller.left = true;
+			controller.forward = false;
+			break;
+		case 38:
+			controller.up = true;
+			break;
         case 39:
-            controller.right = true;
+			controller.right = true;
+			controller.forward = true;
             break;
     }
 }, false);
@@ -68,14 +77,31 @@ document.addEventListener('keyup', function (evt) {
 
 //Bewegung der Figur
 function updatePlayer() {
+
+	if (jumpingOnRightWall()) {
+		main_character.add_jumpingpower += 18
+		console.log(main_character.jumpingpower, main_character.add_jumpingpower);
+		main_character.speed -= 34;
+		main_character.jumping = true;
+		controller.blockPermanentJump = true;
+		controller.forward = false;
+
+	} if (jumpingOnLeftWall()) {
+		main_character.add_jumpingpower += 18
+		main_character.speed += 34;
+		main_character.jumping = true;
+		controller.blockPermanentJump = true;
+		controller.forward = true;
+
+	}
     if (controller.up && main_character.jumping == false && !controller.blockPermanentJump) {
-        main_character.add_jumpingpower += 25;
+        main_character.add_jumpingpower += 22;
         main_character.jumping = true;
         controller.blockPermanentJump = true;
     }
 
     if (controller.left) {
-        main_character.speed -= 1.6;
+        main_character.speed -= 1.2;
     }
 
     if (controller.right) {
@@ -88,13 +114,17 @@ function updatePlayer() {
         main_character.add_jumpingpower = 0;
 	}
 
-    main_character.jumpingpower += main_character.add_jumpingpower;
+	main_character.jumpingpower += main_character.add_jumpingpower;
+	if ((main_character.fallingRight || main_character.fallingLeft) && main_character.jumpingpower <= 0) {
+		main_character.gravity = 0.5;
 
-	main_character.jumpingpower -= 4; //gravity
+	}
+	main_character.jumpingpower -= main_character.gravity; //gravity
 	main_character.x_position += main_character.speed;
     main_character.y_position -= main_character.jumpingpower;
-    main_character.speed *= 0.9; //friction
-    main_character.jumpingpower *= 0.93; //friction
+	main_character.speed *= 0.9; //friction
+	main_character.jumpingpower *= 0.9; //friction
+	main_character.gravity = main_character.normalGravity;
 
     if (main_character.y_position > game_field.height) {
         main_character.alive = false;
@@ -108,24 +138,37 @@ function updatePlayer() {
     if (main_character.x_position > blockSizeX * 31) {
         main_character.speed = 0;
         main_character.x_position = blockSizeX * 31;
-    }
+	}
+	main_character.fallingLeft = false;
+	main_character.fallingRight = false
 };
+
+function jumpingOnLeftWall() {
+    return main_character.fallingLeft && controller.up && controller.left && main_character.jumping && !controller.blockPermanentJump;
+}
+
+function jumpingOnRightWall() {
+    return main_character.fallingRight && controller.up && controller.right && main_character.jumping && !controller.blockPermanentJump;
+}
 
 function drawPlayer() {
     if (main_character.alive) {
-        if (controller.right) {
-            ctx.drawImage(main_character_image, Math.floor(frame_running % 5) * 250, 0, 250, 500, main_character.x_position, main_character.y_position, main_character.width, main_character.height);
-        } else if (controller.left) {
-            ctx.drawImage(main_character_image, Math.floor(frame_running % 5) * 250 + 1250, 0, 250, 500, main_character.x_position, main_character.y_position, main_character.width, main_character.height);
-        } else {
-            ctx.drawImage(main_character_image, 500, 0, 250, 500, main_character.x_position, main_character.y_position, main_character.width, main_character.height);
-        }
+		if (controller.right) {
+			ctx.drawImage(main_character_image, Math.floor(frame_running % 5) * 250, 0, 250, 500, main_character.x_position, main_character.y_position, main_character.width, main_character.height);
+		} else if (controller.left) {
+			ctx.drawImage(main_character_image, Math.floor(frame_running % 5) * 250 + 1250, 0, 250, 500, main_character.x_position, main_character.y_position, main_character.width, main_character.height);
+		} else if (controller.forward) {
+			ctx.drawImage(main_character_image, 500, 0, 250, 500, main_character.x_position, main_character.y_position, main_character.width, main_character.height);
+		}
+		else {
+			ctx.drawImage(main_character_image, 500 + 1250, 0, 250, 500, main_character.x_position, main_character.y_position, main_character.width, main_character.height);
+		}
 
         if (frame_running > 4.84) {
-            frame_running_change = -0.15;
+            frame_running_change = -0.3;
         }
         if (frame_running < 0.01) {
-            frame_running_change = 0.15;
+            frame_running_change = 0.3;
         }
         frame_running += frame_running_change;
 
@@ -136,5 +179,6 @@ function drawPlayer() {
 
 
 function playerNotAutoshifting() {
+	//shiftChange = main_character.speed;
     main_character.x_position -= shiftChange;
 }
